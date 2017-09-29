8b9dd6d13262db61f16f70c58468bb167b6cc6df
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-09-29 11:00:26 +0700

    Bai3 need improvements.

---
+++ Bai1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace Bai1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+			foreach (IPAddress ip in host.AddressList)
+			{
+				Console.WriteLine(ip.ToString());
+			}
+			Console.ReadKey();
+		}
+	}
+}
